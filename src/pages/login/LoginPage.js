d7e78756eefbf7dac04acdf1c92f4a9f3579c658
Your Name <you@example.com>
2022-05-15 10:36:22 +0700

    edit function

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,84 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import ReactLoading from "react-loading";
+import { Link, useNavigate } from "react-router-dom";
+import FormLogin from "../../features/login/FormLogin";
+import serviceCallApi from "../../services/serviceApi";
 
-import { userActions } from '../actions';
+const LoginPage = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
+  const onSubmit = async (data) => {
+    setLoading(true);
+    setLoading(true);
+    try {
+      const result = await serviceCallApi("login", "POST", data);
 
-        // reset login 
-        this.props.logout();
-
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+      setLoading(false);
+      localStorage.setItem("userInfo", JSON.stringify(result.data.data));
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      alert(error.message);
     }
+   
+  };
 
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
-        }
-    }
-
-    render() {
-        const { loggingIn } = this.props;
-        const { username, password, submitted } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        {loggingIn &&
-                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                        <Link to="/register" className="btn btn-link">Register</Link>
-                    </div>
-                </form>
+  return (
+    <section className="h-100">
+      <div className="container h-100">
+        <div className="row justify-content-sm-center h-100">
+          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div className="text-center my-5">
+              <h1>Login </h1>
             </div>
-        );
-    }
-}
-
-function mapState(state) {
-    const { loggingIn } = state.authentication;
-    return { loggingIn };
-}
-
-const actionCreators = {
-    login: userActions.login,
-    logout: userActions.logout
+            <div className="card shadow-lg">
+              <div className="card-body p-5">
+                {loading ? (
+                  <div className="text-center d-flex justify-content-center">
+                    <ReactLoading
+                      type="spin"
+                      color="gray"
+                      height={"20%"}
+                      width={"20%"}
+                    />
+                  </div>
+                ) : (
+                  <FromLogin onSubmit={onSubmit} />
+                )}
+              </div>
+              <div className="card-footer py-3 border-0">
+                <div className="text-center">
+                  Bạn chưa có tài khoản?{" "}
+                  <Link to="/register" className="text-dark">
+                    Register
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
-export { connectedLoginPage as LoginPage };
+export default LoginPage;
---
+++ src/pages/register/RegisterPage.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import ReactLoading from "react-loading";
+import { Link, useNavigate } from "react-router-dom";
+import FromLogin from "../../features/login/FromLogin";
+import serviceCallApi from "../../services/serviceApi";
+import { useForm } from "react-hook-form";
+
+const RegisterPage = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    setLoading(true);
+    try {
+      await serviceCallApi("register", "POST", data);
+      setLoading(false);
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  };
+  return (
+    <section className="h-100">
+      <div className="container h-100">
+        <div className="row justify-content-sm-center h-100">
+          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div className="text-center my-5">
+              <img
+                src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
+                alt="logo"
+                width={100}
+              />
+            </div>
+            <div className="card shadow-lg">
+              <div className="card-body p-5">
+                <h1 className="fs-4 card-title fw-bold mb-4">Register</h1>
+                {loading ? (
+                  <div className="text-center d-flex justify-content-center">
+                    <ReactLoading
+                      type="spin"
+                      color="gray"
+                      height={"20%"}
+                      width={"20%"}
+                    />
+                  </div>
+                ) : (
+                  <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="needs-validation"
+                  >
+                    <div className="mb-3">
+                      <label className="mb-2 text-muted" htmlFor="name">
+                        Name
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-control"
+                        {...register("name", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.name && <span>Name field is required</span>}
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label className="mb-2 text-muted" htmlFor="email">
+                        E-Mail Address
+                      </label>
+                      <input
+                        id="email"
+                        type="email"
+                        className="form-control"
+                        {...register("email", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.name && <span>Email field is required</span>}
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label className="mb-2 text-muted" htmlFor="password">
+                        Password
+                      </label>
+                      <input
+                        id="password"
+                        type="password"
+                        className="form-control"
+                        {...register("password", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.password && (
+                          <span>Bạn phải nhập password</span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label className="mb-2 text-muted" htmlFor="password">
+                        Confirm Password
+                      </label>
+                      <input
+                        id="password"
+                        type="password"
+                        className="form-control"
+                        {...register("c_password", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.c_password && (
+                          <span>Bạn phải nhập lại password</span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="align-items-center d-flex">
+                      <button type="submit" className="btn btn-primary ms-auto">
+                        Register
+                      </button>
+                    </div>
+                  </form>
+                )}
+              </div>
+              <div className="card-footer py-3 border-0">
+                <div className="text-center">
+                  Bạn đã có tài khoản{" "}
+                  <Link to="/login" className="text-dark">
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RegisterPage;
